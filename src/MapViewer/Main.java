de2e369360e0beb42e63c5472bc188691f7b0f76
Ameerul Zul Hadi <ameerul.zulhadi@gmail.com>
2018-11-28 23:52:17 +0800

    Initial commit

---
+++ src/MapViewer/Main.java
@@ -0,0 +1,17 @@
+package MapViewer;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+	
+	public void start(Stage primaryStage) {
+		primaryStage.setTitle("MapViewer");
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
