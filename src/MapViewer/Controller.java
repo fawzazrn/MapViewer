328a500de0c6758bd768eb10d4c5e39834b75667
fawzazrn <fawwazazrin@gmail.com>
2018-12-11 02:22:36 +0800

    Sprite
    Added some codes to accomodate the boat and axe sprites

--- src/MapViewer/Controller.java
+++ src/MapViewer/Controller.java
@@ -1,9 +1,8 @@
 package MapViewer;
 
 import java.awt.image.BufferedImage;
-import java.io.IOException;
-import javax.imageio.ImageIO;
 
+import com.neet.DiamondHunter.Manager.Content;
 import com.neet.DiamondHunter.TileMap.TileMap;
 
 import javafx.embed.swing.SwingFXUtils;
@@ -13,26 +12,32 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
-import javafx.scene.input.MouseEvent;
 
 public class Controller {
 
 	private TileMap m = new TileMap(16);
+	private Model model = new Model();
 	private int tileSize;
 	private Image axe;
 	private Image boat;
+	private int item;
 	
 	int Rownum = 40;
 	int Colnum = 40;
 	@FXML Canvas mapcanvas;
 	@FXML GridPane grid;
+	@FXML AnchorPane grid_anchor_pane;
 	@FXML private Button AxeButton;	//Button initialization used to place the axe
 	@FXML private Button BoatButton; //Button initialization used to place the boat
 	@FXML Label OutputConsoleLabel;	//Text area initialization to test the buttons(not in final prototype) 
 	@FXML Label coLabel;
+	@FXML Label item_label;
 
 	public Controller() {
 		LoadMap();
@@ -44,7 +49,7 @@ public class Controller {
 		int numCols = m.getNumCols();
 		GraphicsContext g = mapcanvas.getGraphicsContext2D();
 		drawMap(g, numRows, numCols);
-		//loadItems();
+		loadItems();
 	}
 	
 	private void LoadMap() {
@@ -63,10 +68,20 @@ public class Controller {
 	}
 
 	public void updateCoords(MouseEvent event) {
-		int x = (int) event.getX() / m.getTileSize();
-		int y = (int) event.getY() / m.getTileSize();
+		double x = (int) event.getX() / m.getTileSize();
+		double y = (int) event.getY() / m.getTileSize();
 		
 		coLabel.setText("(" + x + ", " + y + ")");
+	}
+	
+	public void onMapMouseClick(MouseEvent event) throws Exception {
+		Double xx = event.getX() / m.getTileSize();
+		Double yy = event.getY() / m.getTileSize();
+		
+		if(model.getItemID()==0) {
+			System.out.println(xx + " " + yy);
+			addAxe(Math.round(xx), Math.round(yy));
+		}
 	}
 
 	public void GenGridPane() {		//Function to generate the GridPane
@@ -83,6 +98,55 @@ public class Controller {
 		}
 	}
 
+
+	public void addAxe(double x, double y) {
+		double x_tile = x;
+		double y_tile = y;
+		
+		System.out.println("x tile = " + x_tile + " " + y_tile);
+		if(model.getaxeNumber()<=0) {
+			model.setAxeX(x_tile);
+			model.setAxeY(y_tile);
+			updateMap(x,y,0);
+			model.setaxeNumber(1);
+			
+		}
+		
+	}
+	
+	public void updateMap(double x, double y, int item) {
+		BufferedImage sprite;
+		if(item == 0) {
+			sprite = Content.ITEMS[1][0];
+		}
+		
+		else {
+			sprite = Content.ITEMS[1][1];
+		}
+		
+		//converts BUFFEREDIMAGE to an image
+		ImageView item_image_view = new ImageView();
+		Image spriteImage = SwingFXUtils.toFXImage(sprite, null);
+		item_image_view.setImage(spriteImage);
+		
+		grid_anchor_pane.getChildren().add(item_image_view);
+		item_image_view.setX(x);
+		item_image_view.setY(y);
+		
+		
+	}
+	
+	private void loadItems() {
+		
+		AxeButton.setOnAction((event) -> {
+			item=0; //item selected is the axe
+			System.out.println("Axe is selected");
+			model.setItemID(item);
+		});
+	}
+	
+	
+	/*
 	private void loadItems(String location) {
 		BufferedImage items;
 		try {
@@ -92,6 +156,9 @@ public class Controller {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}
+	}*/
+	
+	
+
 
 }
--- src/MapViewer/Model.java
+++ src/MapViewer/Model.java
@@ -2,17 +2,70 @@ package MapViewer;
 
 public class Model {
 
-	private double boat_x;
-	private double boat_y;
-	private double axe_x;
-	private double axe_y;
+	private double boatx;
+	private double boaty;
+	private double axex;
+	private double axey;
+	private double item;
 	
 	private double old_boat_x;
 	private double old_boat_y;
 	private double old_axe_x;
 	private double old_axe_y;
 	
+	private double boatNumber;
+	private double axeNumber;
 	
+	private int oldboatNumber;
+	private int oldaxeNumber;
+	
+	public void setBoatX(double bx) {
+		boatx = bx;
+	}
+	
+	public void setBoatY(double by) {
+		boaty = by;
+	}
+	
+	public void setAxeX(double ax) {
+		axex = ax;
+	}
+	
+	public void setAxeY(double ay) {
+		axey = ay;
+	}
+	
+	public double getAxeX() {
+		return axex;
+	}
+	
+	public double getAxeY() {
+		return axey;
+	}
+	
+	public double getBoatX() {
+		return boatx;
+	}
+	
+	public double getBoatY() {
+		return boaty;
+	}
+	
+	public void setaxeNumber(double axeNumber) {
+		this.axeNumber = axeNumber;
+	}
+	
+	public double getaxeNumber() {
+		return axeNumber;
+	}
+	
+	public void setItemID(double item) {
+		this.item = item;
+	}
+	
+	public double getItemID() {
+		return item;
+	}
 	
 	
 }
